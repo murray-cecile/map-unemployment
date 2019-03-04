594c751c1843d2450032c99bbadb82037247ce78
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-03-03 22:14:32 -0600

    submitting here: fixed bar charts and rotated rug

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // CECILE MURRAY
+// Draws on https://github.com/cmgiven/gap-reminder-v4
+// and on the various exercises/solutions in https://github.com/mcnuttandrew/capp-30239
+// and on https://github.com/ivan-ha/d3-hk-map/blob/development/map.js
 
 
 // handy fn to compute domain
@@ -14,12 +17,12 @@ function computeDomain(data, key) {
 // RUG
 function makeRug(urates) {
 
-  const width = 800;
-  const height = 75;
+  const width = 75;
+  const height = 600;
   const margin = {
     top: 10,
-    left: 20,
-    right: 20,
+    left: 10,
+    right: 10,
     bottom: 10
   };
 
@@ -27,13 +30,13 @@ function makeRug(urates) {
           .attr('width', margin.left + width + margin.right)
           .attr('height', margin.top + height + margin.bottom);
   
-  const xScale = d3.scaleLinear()
-                   .domain([0, d3.max(urates, p => p.adj_urate)])
-                   .range([margin.left, width]);
-  
   const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(urates, p => p.adj_urate)])
+                   .range([height, margin.bottom]);
+  
+  const xScale = d3.scaleLinear()
                    .domain([0, 1])
-                   .range([0, height]);
+                   .range([0, width]);
   
   const urateScale = d3.scaleLinear()
                     .domain([0, d3.max(urates, p => p.adj_urate)])
@@ -45,17 +48,17 @@ function makeRug(urates) {
     .data(urates)
     .enter()
     .append('rect')
-    .attr('x', d => xScale(d.adj_urate))
-    .attr('y', yScale(0.5))
-    .attr('width', 2)
-    .attr('height', 40)
+    .attr('x', xScale(0.5))
+    .attr('y', d => yScale(d.adj_urate))
+    .attr('width', 40)
+    .attr('height', 0.5)
     .attr('fill', d => colorScale(d.adj_urate));                 
 };
 
-// MAP!
+// MAP
 function makeMap(shp, urates) {
 
-  const width = 900;
+  const width = 850;
   const height = 600;
   const margin = {
     top: 20,
@@ -81,16 +84,27 @@ function makeMap(shp, urates) {
 
   const colorScale = d => d3.interpolateViridis(urateScale(d));
   
+
+  function mouseOverHandler(d) {
+    d3.select(this).attr('fill', "#FFD816");
+  };
+
+  function mouseOutHandler(d) {
+    d3.select(this).attr("fill", d => colorScale(fips2Value[d.properties.GEOID]));
+  };
+
   svg.selectAll('path')
     .data(shp.features)
     .enter()
     .append('path')
     .attr('d', d => geoGenerator(d))
-    .attr('fill', d => colorScale(fips2Value[d.properties.GEOID]));
+    .attr('fill', d => colorScale(fips2Value[d.properties.GEOID]))
+    .on("mouseover", mouseOverHandler)
+    .on("mouseout", mouseOutHandler);
 
 };
 
-function makeIndustryBar(industry_data, which_bar) {
+function makeNatlBar(industry_data, which_bar) {
   const width = 400;
   const height = 400;
   const margin = {
@@ -125,36 +139,41 @@ function makeIndustryBar(industry_data, which_bar) {
 
 };
 
-IndustryBar = function () {
-  this.setup();
+IndustryBar = function (selector, industry_data) {
+  yearData = industry_data.filter(d => d.year === app.globals.selected.year);
+  // console.log(yearData);
+  this.setup(selector, yearData);
 };
 
 IndustryBar.prototype = {
-  setup: function () {
+  
+  setup: function (selector, industry_data) {
       chart = this;
 
-      margin = { top: 10, right: 10, bottom: 10, left: 20 };
+      margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
       width = 400 - margin.left - margin.right;
       height = 400 - margin.top - margin.bottom;
 
-      chart.svg = d3.select('#bar2')
+      chart.svg = d3.select(selector)
           .append('svg')
           .attr('width', width + margin.left + margin.right)
           .attr('height', height + margin.top + margin.bottom)
           .append('g')
           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-      const industries = [ ... new Set(app.data.cty_industry.map(x => x.industry_name))]; // https://codeburst.io/javascript-array-distinct-5edc93501dc4
+      const industries = [ ... new Set(industry_data.map(x => x.industry_name))]; // https://codeburst.io/javascript-array-distinct-5edc93501dc4
   
-
       chart.scales = {
           x: d3.scaleBand()
                .domain(industries)
                .range([margin.left, width]),
           y: d3.scaleLinear()
-              .domain([0, d3.max(app.data.cty_industry, d => d.industry_share)])
-              .range([height, 0])
+              .domain([0, d3.max(industry_data, d => d.industry_share)])
+              .range([height, 0]),
+          height: d3.scaleLinear()
+              .domain([0, d3.max(industry_data, d => d.industry_share)])
+              .range([0, height])
       };
 
       xAxis = d3.axisBottom().scale(chart.scales.x);
@@ -174,22 +193,22 @@ IndustryBar.prototype = {
       //     .attr('y', 0)
       //     .attr('class', 'tooltip')
 
-      chart.update();
+      chart.update(selector);
   },
 
-  update: function () {
+  update: function (selector) {
       chart = this;
 
-      yearData = app.data.cty_industry.filter(d => d.year === app.globals.selected.year);
+      console.log(yearData);
 
-      chart.svg.selectAll('rect')
+      chart.svg.selectAll(selector + ' rect')
           .data(yearData)
           .enter()
           .append('rect')
           .attr('x', d => chart.scales.x(d.industry_name))
-          .attr('y', d => chart.scales.y(d.industry_share))
-          .attr('width', d => chart.scales.x.bandwidth())
-          .attr('height', d => chart.scales.y(d.industry_share))
+          .attr('y', d => height - chart.scales.height(d.industry_share))
+          .attr('width', chart.scales.x.bandwidth())
+          .attr('height', d => chart.scales.height(d.industry_share))
           .attr('fill', "#217FBE");
 
   }
@@ -201,7 +220,8 @@ app = {
 
   globals: {
       // available: { years: d3.range(MIN_YEAR, MAX_YEAR + 1) },
-      selected: { year: 2017 },
+      selected: { date: "2017-12",
+                  year: 2017 },
       animating: false
   },
 
@@ -217,8 +237,8 @@ app = {
 
       app.components.Rug = makeRug(app.data.urates);
       app.components.Map = makeMap(app.data.shp, app.data.urates);
-      app.components.natlBar = makeIndustryBar(natl_industry, '#bar1');
-      app.components.industryBar = new IndustryBar('#bar2');
+      app.components.natlBar = new IndustryBar('#bar1', app.data.natl_industry);
+      app.components.ctyBar = new IndustryBar('#bar2', app.data.cty_industry);
       // app.components.controls    = new Controls('#controls')
 
 
@@ -280,15 +300,3 @@ Promise.all([
   // .then(data => makeCharts(data));
   .then(data => app.initialize(data));
 
-
-function makeCharts(data) {
-
-  const [shp, urates, natl_industry, cty_industry] = data;
-
-  console.log(cty_industry);
-
-  makeRug(urates);
-  makeMap(shp, urates);
-  makeIndustryBar(natl_industry, '#bars :first-child');
-  // makeIndustryBar(cty_industry, '#bars :last-child');
-}
